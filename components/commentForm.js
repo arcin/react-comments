684a9fd14258a5266cd6138e3c9d0b657c0c0894
Charles Arciniega <charlesarcin@gmail.com>
2015-02-03 11:27:18 -0800

    Add commentForm and commentList skeletons

---
+++ components/commentForm.js
@@ -0,0 +1,11 @@
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      React.createElement(
+        'div',
+        { className: "commentForm" },
+        "I'm a comment form"
+      )
+    )
+  }
+})
---
+++ components/commentList.js
@@ -0,0 +1,11 @@
+var CommentList = React.createClass({
+  render: function(){
+    return(
+      React.createElement(
+        'div',
+        {className: "commentList"},
+        "I'm a commentList"
+      )
+    )
+  }
+})
