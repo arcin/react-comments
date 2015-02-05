8821de221a8400df5c092a9532a882548f0c649d
Charles Arciniega <charlesarcin@gmail.com>
2015-02-04 17:31:55 -0800

    Create a form in the VDOM
      - Use refs to give the CommentForm component reference to its child
        elements.
      - Use `getDOMNode()` to grab the native browser DOM element.
      - Attach a submit event listener to our form submit button.
      - Clear the form on submit.

--- public/scripts/components/commentForm.js
+++ public/scripts/components/commentForm.js
@@ -1,5 +1,25 @@
 define(function(){
   var CommentForm = React.createClass({
+    handleSubmit: function(e) {
+      e.preventDefault()
+      var author = this.refs.author.getDOMNode().value.trim()
+      var text = this.refs.text.getDOMNode().value.trim()
+
+      if (!text || !author) {
+        return
+      }
+
+      /* TODO: send request to the server */
+
+      /*
+       * `this.refs.author` references a React Component.
+       * You can call `getDOMNode()` on a React Component
+       * to get its native browser DOM element.
+       * */
+      this.refs.author.getDOMNode().value = ''
+      this.refs.text.getDOMNode().value = ''
+      return
+    },
     render: function(){
       return (
         // Create a single VDOM node.
@@ -7,11 +27,37 @@ define(function(){
         //  1. The node type. Can be an HTML node or a Component.
         //  2. The attributes associated with the node.
         //  3. The children of this node
-        React.createElement(
-          'div',
-          { className: "commentForm" },
-          "I'm a comment form"
-        ))}})
+
+        /*
+         * Create a form for comment submission in the VDOM
+         *
+         * React attaches event handlers to components using a CamelCase naming
+         * convention. ex: onSubmit.
+         * */
+        React.createElement( 'form', { className: "commentForm", onSubmit: this.handleSubmit }, [
+          React.createElement( 'input', {
+            type: 'text',
+            placeholder: 'Your name',
+            /*
+             * React elements have 4 properties. Type, props, keys, and ref.
+             * Here we are setting a `ref` on a child element to access it
+             * later
+             */
+            ref: 'author'
+          }),
+          React.createElement( 'input', {
+            type: 'text',
+            placeholder: 'Say something...',
+            /*
+             * React elements have 4 properties. Type, props, keys, and ref.
+             * Here we are setting a `ref` on a child element to access it
+             * later
+             */
+            ref: 'text'
+          }),
+          React.createElement( 'input', { type: 'submit', value: 'Post' })
+        ]
+      ))}})
 
   // export this component
   return CommentForm
